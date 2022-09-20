c5de1b75a27d16a007aa5c028b4eb058ec1ad8fb
samridhi <83155658+Shaurya2712@users.noreply.github.com>
2022-09-20 23:04:49 +0530

    some changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function About() {
   return (
-      <p>Throw in some about info here </p>
+    <>
+      <p className="text-center fs-1 text-dark">
+        {" "}
+        here we are making a webapp for music using api from spotify{" "}
+      </p>
+      <h5 className="text-info text-center">all rights reserved !</h5>
+      
+    </>
   );
 }
 
-export default About
+export default About;
--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./footer.css";
 
-function footer (){
-    return(
-        <footer>
-            <div>
-                <h1 style={{fontSize: "50px"}}><b>Thanks For Using The Site</b></h1>
-            </div>
-            <span style={{fontSize: "20px"}}>Copyright @ TEAM MUSICERS 2022</span>
-            <div>
-                <p>All Rights Reserved</p>
-            </div>
-        </footer>
-    )
+function footer() {
+  return (
+    <footer className="bg-secondary">
+      <div>
+        <h1 style={{ fontSize: "50px" }} className="text-dark  ">
+          <b>Thanks For Using The Site</b>
+        </h1>
+      </div>
+      <span style={{ fontSize: "20px" }}>Copyright @ TEAM MUSICERS 2022</span>
+      <div>
+        <p>All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 }
 
-export default footer
+export default footer;

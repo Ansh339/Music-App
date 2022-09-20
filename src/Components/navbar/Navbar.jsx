8f933e79c47a5772de8ef38496f54e38ea4d5228
samridhi <78554789+samridhi14@users.noreply.github.com>
2022-09-20 22:06:18 +0530

    updated navbar
    made some changes
    repaired the hamburger icon

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -4,33 +4,71 @@ import React from "react";
 import { Component } from "react";
 
 class Navbar extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src={image} alt="Music" height='25%' width='25%' /></a>
-            <div class="collapse navbar-collapse" id="navbarColor01">
-              <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                  <a class="nav-link"  onClick={(event) => {this.props.setNav(1)}}>Home
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <img src={image} alt="Music" height="25%" width="25%" />
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarColor01"
+              aria-controls="navbarColor01"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+              <ul className="navbar-nav ">
+                <li className="nav-item ">
+                  <a
+                    className="nav-link text-dark  fs-4 "
+                    onClick={(event) => {
+                      this.props.setNav(1);
+                    }}
+                  >
+                    Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link"  onClick={(event) => {this.props.setNav(2)}}>Library
+                <li className="nav-item">
+                  <a
+                    className="nav-link text-dark fs-4"
+                    onClick={(event) => {
+                      this.props.setNav(2);
+                    }}
+                  >
+                    Library
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link"  onClick={(event) => {this.props.setNav(3)}}>About
+                <li className="nav-item ">
+                  <a
+                    className="nav-link text-dark  fs-4"
+                    onClick={(event) => {
+                      this.props.setNav(3);
+                    }}
+                  >
+                    About
                   </a>
                 </li>
               </ul>
-              <form class="d-flex">
-                <input class="form-control me-sm-2" type="text" placeholder="Search" value={this.props.trackName} onChange = {(event) => {this.props.setTrackName(event.target.value)}}/>
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+              <form className="d-flex justify-content-end">
+                <input
+                  className="form-control me-sm-2  "
+                  type="text"
+                  placeholder="Search"
+                  value={this.props.trackName}
+                  onChange={(event) => {
+                    this.props.setTrackName(event.target.value);
+                  }}
+                />
               </form>
             </div>
           </div>

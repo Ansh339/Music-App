2cebcab20d2a908d227302d767d1c0d0a6f10f59
AdityaChanan <83080295+AdityaChanan@users.noreply.github.com>
2022-09-22 10:20:14 +0530

    New and cleaner src folder

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h4 className="text-center fs-1 text-dark">Purpose</h4>
+      <p className="text-center fs-1 text-dark mx-5 my-5">
+        This Project is our Attempt as a team to create a Music App using the
+        API provided by Spotify to fetch the data of the various Music Tracks
+      </p>
+      <h1 className="text-dark text-center">TEAM-</h1>
+      <h4 className=" text-uppercase text-dark text-center">Aditya</h4>
+      <h4 className=" text-uppercase text-dark text-center">Ansh</h4>
+      <h4 className=" text-uppercase text-dark text-center">Chaya</h4>
+      <h4 className=" text-uppercase text-dark text-center">Devangini</h4>
+      <h4 className=" text-uppercase text-dark text-center">Samridhi</h4>
+      <h4 className=" text-uppercase text-dark text-center">Shaurya</h4>
+    </>
+  );
+}
+
+export default About;

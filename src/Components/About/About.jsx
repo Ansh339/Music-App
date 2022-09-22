8026f5d820e83be0b1bfe813cff4a131ed182eb1
Ansh <90087682+Ansh339@users.noreply.github.com>
2022-09-22 11:03:03 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,18 +3,24 @@ import React from "react";
 function About() {
   return (
     <>
-      <h4 className="text-center fs-1 text-dark">Purpose</h4>
-      <p className="text-center fs-1 text-dark mx-5 my-5">
-        This Project is our Attempt as a team to create a Music App using the
-        API provided by Spotify to fetch the data of the various Music Tracks
-      </p>
-      <h1 className="text-dark text-center">TEAM MUSICERS</h1>
-      <h4 className=" text-uppercase text-dark text-center">Aditya</h4>
-      <h4 className=" text-uppercase text-dark text-center">Ansh</h4>
-      <h4 className=" text-uppercase text-dark text-center">Chaya</h4>
-      <h4 className=" text-uppercase text-dark text-center">Devangini</h4>
-      <h4 className=" text-uppercase text-dark text-center">Samridhi</h4>
-      <h4 className=" text-uppercase text-dark text-center">Shaurya</h4>
+      <div>
+        <h1 className="text-center fs-1 text-dark">PURPOSE</h1>
+        <h5 className="text text-dark text-center">
+          This Project is our Attempt as a Team to create a Music App using the
+          API provided by Spotify to fetch the data of Music Tracks available and display them according to the basis of the User that has logged onto our Site. 
+        </h5>
+      </div>
+      <br />
+      <br />
+      <div>
+        <h1 className="text-dark text-center">TEAM MUSICERS</h1>
+        <h4 className=" text-uppercase text-dark text-center">Aditya</h4>
+        <h4 className=" text-uppercase text-dark text-center">Ansh</h4>
+        <h4 className=" text-uppercase text-dark text-center">Chaya</h4>
+        <h4 className=" text-uppercase text-dark text-center">Devangini</h4>
+        <h4 className=" text-uppercase text-dark text-center">Samridhi</h4>
+        <h4 className=" text-uppercase text-dark text-center">Shaurya</h4>
+      </div>
     </>
   );
 }

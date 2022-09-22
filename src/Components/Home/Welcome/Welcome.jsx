34f19931141654e8ef6daac42df8aa1e4b41f63e
Ansh <90087682+Ansh339@users.noreply.github.com>
2022-09-22 10:44:43 +0530

    Update Welcome.jsx

--- src/Components/Home/Welcome/Welcome.jsx
+++ src/Components/Home/Welcome/Welcome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Welcome = ({ userName }) => {
 
     return (
-        <h2 style={{fontWeight: 'bolder', color: 'yellow', textAlign: 'center', fontSize: '4em'}}><b>Hi There {userName}! </b></h2>
+        <h2 style={{fontWeight: 'bolder', textAlign: 'center', fontSize: '4em'}}><b>Hi There {userName}! </b></h2>
     )
 }
 
